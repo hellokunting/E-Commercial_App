a70e4b8310f5fe33ef321923b2948df4d9d410a9
hellokunting <hellokunting@gmail.com>
2021-08-14 23:16:35 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+// import directories 
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import Cart from './CartPage'
+import About from './AboutPage'
+import PrivateRoute from './PrivateRoute'
+import AuthWrapper from './AuthWrapper'
+
+export{ Home, Products, SingleProduct, Error, Checkout, Cart, About, PrivateRoute,AuthWrapper }
---
+++ src/testing.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+background: green;
+color: white;
+`
+const Container = styled.div`
+  background: 
+
+`
+
+const testing = () => {
+    return (
+        <Wrapper>
+            <h3>Hello World</h3>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.section`
+background: black;
+`
+
+export default testing
